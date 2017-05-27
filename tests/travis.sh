5dae5c1a7cca2d3c9e73c921e8ae6a59adfd60f0
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-05-27 16:47:28 +0200

    Update travis.sh

--- tests/travis.sh
+++ tests/travis.sh
@@ -10,19 +10,12 @@ while getopts "p:" OPTION 2> /dev/null; do
 	esac
 done
 
-./tests/lint.sh -p "$PHP_BINARY"
-
-if [ $? -ne 0 ]; then
-	echo Lint scan failed!
-	exit 1
-fi
-
 cp -r tests/plugins plugins
 "$PHP_BINARY" ./plugins/PocketMine-DevTools/src/DevTools/ConsoleScript.php --make ./plugins/PocketMine-DevTools --relative ./plugins/PocketMine-DevTools --out ./plugins/DevTools.phar
 rm -rf ./plugins/PocketMine-DevTools
 
 echo -e "version\nmakeserver\nstop\n" | "$PHP_BINARY" src/pocketmine/PocketMine.php --no-wizard --disable-ansi --disable-readline --debug.level=2
-if ls plugins/DevTools/Rocky_1.0.2.phar >/dev/null 2>&1; then
+if ls plugins/DevTools/Rocky.phar >/dev/null 2>&1; then
     echo Server phar created successfully.
 else
     echo No phar created!
